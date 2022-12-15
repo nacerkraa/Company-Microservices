3bdd7ddba89c4fcc2deb0a74a1c0533a7d3677b6
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-12-15 01:09:10 +0100

    rename the packages

--- src/main/java/com/example/microservices/entity/Company.java
+++ src/main/java/com/example/microservices/entity/Company.java
@@ -1,11 +1,20 @@
-package com.example.SpringCloud.entity;
+package com.example.microservices.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
+// Lamboc annotations
+@Data // pour les getters et les setters
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Company {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY) // for auto increments
  	private Long id;
